6c1a5133da667fce8abf9b188b67b02027b3ffff
Muhammad Fajar Nurjaman <fajarnurjaman25@gmail.com>
2026-05-27 01:38:48 +0700

    Memperbaiki Variable Color pada app_theme.dart

--- mobile/lib/app/theme/app_theme.dart
+++ mobile/lib/app/theme/app_theme.dart
@@ -4,21 +4,21 @@ class AppTheme {
   static ThemeData light() {
     const colorScheme = ColorScheme(
       brightness: Brightness.light,
-      primary: navy,
-      onPrimary: white,
-      secondary: orange,
-      onSecondary: white,
-      surface: white,
-      onSurface: navy,
+      primary: Colors.blue,
+      onPrimary: Colors.white,
+      secondary: Colors.orange,
+      onSecondary: Colors.white,
+      surface: Colors.white,
+      onSurface: Colors.blue,
       error: Colors.red,
-      onError: white,
-      tertiary: orange,
-      onTertiary: white,
+      onError: Colors.white,
+      tertiary: Colors.orange,
+      onTertiary: Colors.white,
     );
 
     return ThemeData(
       useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(seedColor: seed),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       appBarTheme: const AppBarTheme(centerTitle: false),
       inputDecorationTheme: const InputDecorationTheme(
         border: OutlineInputBorder(),
